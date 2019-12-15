d9ad67da983d89f9c42009f7e3b14076f8ccf915
polynomial space [MBP] <git@polynomial.space>
2019-12-15 13:23:42 -0700

    1p2 done in C

---
+++ c/1p2/2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h> // atoi
+
+int getfuel(int mass) {
+	int fuel;
+	fuel = (mass / 3) - 2; // typical div should handle rounding down
+
+	return fuel;
+}
+
+int getcumulativefuel(int mass) {
+	int cum, total;
+
+	total = 0;
+	cum = getfuel(mass);
+	for (;cum > 0; cum = getfuel(cum)) {
+		total += cum;
+	}
+
+	return total;
+}
+
+int main(int argc, char **argv) {
+	int fuel, mass;
+	char *line;
+	ssize_t linelen;
+	size_t linecap;
+	unsigned long long total;
+	FILE *fp;
+
+	fp = fopen("./input", "r");
+	if (!fp) {
+		fprintf(stderr, "Cannot open input file\n");
+		return 1;
+	}
+
+	total = 0;
+	for(linelen = 0, line = NULL; (linelen = getline(&line, &linecap, fp)) > 0; ) {
+		mass = atoi(line);
+		fuel = getcumulativefuel(mass);
+		//printf("%d\n", fuel);
+		total += fuel;
+
+		linelen = 0;
+		line = NULL;
+	}
+
+	printf("%llu\n", total);
+	return 0;
+}
