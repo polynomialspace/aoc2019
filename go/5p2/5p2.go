37f53950d57d536f626a5f4bfc5632bb8b67ad68
polynomial space [MBP] <git@polynomial.space>
2020-04-06 16:21:20 -0600

    partial refactor

--- go/5p2/5p2.go
+++ go/5p2/5p2.go
@@ -10,223 +10,207 @@ import (
 )
 
 const (
-	Add = 1  // ArgPos , ArgPos, ResPos
-	Mul = 2  // ArgPos , ArgPos, ResPos
-	Inp = 3  // Ext In , ResPos
-	Out = 4  // Ext Out, ArgPos
-	JIT = 5  // Jmp True
-	JIF = 6  // Jmp False
-	TLT = 7  // ArgPos , ArgPos, ResPos (Bool)
-	TEQ = 8  // ArgPos , ArgPos, ResPos (Bool)
+	Add = 1  // Add
+	Mul = 2  // Multiply
+	Inp = 3  // receive user Input
+	Out = 4  // send Output to user
+	JIT = 5  // Jump If True
+	JIF = 6  // Jump If False
+	TLT = 7  // Test Less Than
+	TEQ = 8  // Test EQuals
 	
-	Hlt = 99 // -
+	Hlt = 99 // Halt
 )
 
-type Instruction func (a int, b int, out *int, off *int)
+type Instruction func (arg ...int) (out *int)
 
 type op struct {
-	opcode int
-	inst Instruction //closure to execute instruction
-	off  *int  // offset in operation list, effective instptr
+	inst Instruction // lambda to execute instruction
 	len  int
-	mode []int // Immediate, Position
 	argp []int // argument position list
 }
 
-func opgetmode(o op) (op) {
-	// drop opcode itself, we only care about mode
-	opcode := o.opcode
-	opcode /= 100
 
-	umod := intsplit(uint(opcode))
-	smod := make([]int, 3)
-	for i, m := range umod {
-		smod[(len(umod)-1)-i] = int(m)
+func intsplit(i int) (split []int) {
+	for i > 1 {
+		split = append(split, i % 10)
+		i /= 10
 	}
-	o.mode = smod
-
-	return o
-}
-
-func intsplit(u uint) (out []uint) {
-	for u > 1 {
-		out = append([]uint{u % 10}, out...)
-		u /= 10
-	}
-	out = append([]uint{u % 10}, out...)
+	split = append(split, i % 10)
 
 	return
 }
 
-func intrev(s []int) ([]int) {
-	l := len(s)-1
+func mode(intcode int) ([]int) {
+	// drop intcode itself, we only care about mode
+	intcode /= 100
 
-	for i := 0; i < l/2; i++ {
-		s[i],s[l-i] = s[l-i],s[i]
+	mode := make([]int, 3)
+	for i, m := range intsplit(intcode) {
+		mode[i] = m
 	}
 
-	return s
+	return mode
 }
 
-func instrexec(o op, oplist []int) {
-	out := o.argp[len(o.argp)-1]
-	a := oplist[o.argp[0]]
-	b := 0
-	off := o.off
-	if len(o.argp) > 1 {
-		b = oplist[o.argp[1]]
+func (o op) exec (oplist []int) {
+	outp := o.argp[len(o.argp)-1]
+
+	args := make([]int, len(o.argp))
+	for i, val := range o.argp {
+		args[i] = oplist[val]
 	}
-	o.inst(a, b, &(oplist[out]), off)
+
+	if ret := o.inst(args...); ret != nil {
+		oplist[outp] = *ret
+	}
 }
 
-func opgetargspos(o op, oplist []int) (op) {
+func (o op) argspos(oplist []int, mode []int, offset int) (op) {
+	const (
+		position  = 0
+		immediate = 1
+	)
+
 	args := make([]int, o.len - 1)
-	//m := intrev(o.mode)
-	m := o.mode
-	offset := *(o.off)
 
-	for i := 0; i < len(args); i++ {
+	for i := range args {
 		pos := 1 + offset + i
-		if m[i] == 1 {
-			// immediate mode
+
+		switch mode[i] {
+		case immediate:
 			args[i] = pos
-		} else {
-			// position mode
-			argpos := oplist[pos]
-			args[i] = argpos
+		case position:
+			args[i] = oplist[pos]
 		}
 	}
+
 	o.argp = args
 
 	return o
 }
 
-func opgetfn(o op) (op) {
-	length := 0
-	opcode := o.opcode % 100
+func fn(intcode int, off *int) (Instruction, int) {
+	intcode %= 100 // drop mode
 
 	var inst Instruction
-	switch opcode {
+	var length int
+
+	switch intcode {
 	case Hlt:
 		inst = nil
 	case Add:
-		inst = func(a int, b int, out *int, off *int) {
-			*out = a + b
+		inst = func (arg ...int) (*int) {
+			out := arg[0] + arg[1]
+			return &out
 		}	
 		length = 4
 	case Mul:
-		inst = func(a int, b int, out *int, off *int) {
-			*out = a * b
+		inst = func (arg ...int) (*int) {
+			out := arg[0] * arg[1]
+			return &out
 		}
 		length = 4
 	case Inp:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: a, b, off
+		inst = func (arg ...int) (*int) {
 			scanner := bufio.NewScanner(os.Stdin)
 			fmt.Printf("INPUT> ")
 			scanner.Scan()
-			s := scanner.Text()
-			in, _ := strconv.Atoi(s)
-			*out = in
-			fmt.Printf("Inp: Wrote %d\n", *out)
+			out, _ := strconv.Atoi(scanner.Text())
+			return &out
 		}
 		length = 2
 	case Out:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: b, out, off
-			fmt.Printf("OUTPUT: %d\n", a)
+		inst = func (arg ...int) (*int) {
+			fmt.Printf("OUTPUT: %d\n", arg[0])
+			return nil
 		}
 		length = 2
 	case JIT:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: out
-			if a != 0 {
-				*off = b
+		inst = func (arg ...int) (*int) {
+			if arg[0] != 0 {
+				*off = arg[1]
 			}
+			return nil
 		}
 		length = 3
 	case JIF:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: out
-			if a == 0 {
-				*off = b
+		inst = func (arg ...int) (*int) {
+			if arg[0] == 0 {
+				*off = arg[1]
 			}
+			return nil
 		}
 		length = 3
 	case TLT:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: off
-			if a < b {
-				*out = 1
+		inst = func (arg ...int) (*int) {
+			var out int
+			if arg[0] < arg[1] {
+				out = 1
 			} else {
-				*out = 0
+				out = 0
 			}
+			return &out
 		}
 		length = 4
 	case TEQ:
-		inst = func(a int, b int, out *int, off *int) {
-			//unused: off
-			if a == b {
-				*out = 1
+		inst = func (arg ...int) (*int) {
+			var out int
+			if arg[0] == arg[1] {
+				out = 1
 			} else {
-				*out = 0
+				out = 0
 			}
+			return &out
 		}
 		length = 4
 	default:
 		inst = nil
 	}
 
-	o.len = length
-	o.inst = inst
-
 	if inst == nil {
-		fmt.Printf("Halting with opcode %d\n", opcode)
+		fmt.Printf("Halting with opcode %d\n", intcode)
 	}
 
-	return o
+	return inst, length
 }
 
-func opparse(oplist []int, offset *int) (opcode op, halt bool){
-	o := op{off: offset}
-	o.opcode = oplist[*offset]
-	o = opgetfn(o)
+func parse(intcodes []int, offset *int) (opcode op, halt bool){
+	var o op
+	intcode := intcodes[*offset]
+	o.inst, o.len = fn(intcode, offset)
 	if o.inst == nil {
-		return op{}, false
+		return op{}, true
 	}
-	o = opgetmode(o)
-	o = opgetargspos(o, oplist)
+	o = o.argspos(intcodes, mode(intcode), *offset)
 
 	*offset += o.len
-	
-	return o, true
+	return o, false
 }
 
 func main() {
 	file, err := os.Open("./input")
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	
-	var in []string
-	for scanner.Scan() {
+	in := []string{}
+	for scanner := bufio.NewScanner(file); scanner.Scan(); {
 		in = strings.Split(scanner.Text(), ",")
 	}
 
-	oplist := make([]int, len(in))
-
+	intcodes := make([]int, len(in))
 	for i,s := range in {
-		oplist[i], _ = strconv.Atoi(s)
+		intcodes[i], _ = strconv.Atoi(s)
 	}
 
 	off := 0
-	o, hlt := opparse(oplist, &off)
-	for hlt {
-		instrexec(o, oplist)
-		o, hlt = opparse(oplist, &off)
+	o, hlt := parse(intcodes, &off)
+	for !hlt {
+		o.exec(intcodes)
+		o, hlt = parse(intcodes, &off)
 	}
 
 }
